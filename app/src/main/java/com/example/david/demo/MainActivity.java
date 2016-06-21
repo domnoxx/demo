2f82681a721db23010bb2d7233901b3e0bb64750
domnoxx <d-avis89@hotmail.es>
2016-06-21 05:14:41 -0500

    Update

--- app/src/main/java/com/example/david/demo/MainActivity.java
+++ app/src/main/java/com/example/david/demo/MainActivity.java
@@ -30,60 +30,75 @@ public class MainActivity extends AppCompatActivity {
     private Button btnEnviar;
     private LinearLayout fondo;
     private EditText txtComentario;
+    private String sim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+       // this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         txtNombre = (EditText) findViewById(R.id.txtNombre);
         atxtMateria= (AutoCompleteTextView) findViewById(R.id.atxtMateria);
          calificacion = (RatingBar) findViewById(R.id.ratingBar);
         ckbMostrar=(CheckBox) findViewById(R.id.ckbRating);
         fondo=(LinearLayout)findViewById(R.id.Pantalla);
-        color=(ToggleButton)findViewById(R.id.tgbColor);
-        txtComentario=(EditText)findViewById(R.id.txtComentario);
-        color.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-
-                   fondo.setBackgroundColor(Color.BLUE);
-                } else {
-                    // The toggle is disabled
-                    fondo.setBackgroundColor(Color.WHITE);
-                }
-            }
-        });
+        //color=(ToggleButton)findViewById(R.id.tgbColor);
+        //setColor(color);
 
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_dropdown_item_1line, MATERIAS);
+
+
         //atxtMateria = (AutoCompleteTextView) findViewById(R.id.atxtMateria);
         atxtMateria.setThreshold(3);
         atxtMateria.setAdapter(adapter);
     }
 
 
+
+
+        //});
+    //}
     private static final String[] MATERIAS = new String[] {
             "Seguridades de software", "Administrador de contenido", "Aplicaciones Moviles"
     };
 
 
+    public void mostrar(View v){
+        if(ckbMostrar.isChecked())
+        {
+            sim="si";
+            System.out.println("si");
+        }else{
+            sim="no";
+            System.out.println("no");
+        }
+
+    }
     public void publicar(View v) {
         Intent demo = new Intent(this, Publicacion.class);
         demo.putExtra("nombre", txtNombre.getText().toString());
         //out.print(atxtMateria.toString());
-        demo.putExtra("Materia",String.valueOf(atxtMateria.getText().toString()));
-        demo.putExtra("Comentario", txtComentario.getText().toString());
+       // demo.putExtra("Materia",String.valueOf(atxtMateria.getText().toString()));
         demo.putExtra("Calificacion",String.valueOf(calificacion.getRating()));
 
-        demo.putExtra("Color",color.getText());
+        if(ckbMostrar.isChecked())
+        {
+            sim="si";
+            demo.putExtra("Mostrar", sim);
+        }else{
+            sim="no";
+            demo.putExtra("Mostrar", sim);
+        }
 
-        demo.putExtra("mostrar", ckbMostrar.getText().toString());
+
+       // demo.putExtra("mostrar", ckbMostrar.getText().toString());
 
         startActivity(demo);
 
     }
+
 
 
 
--- app/src/main/java/com/example/david/demo/Publicacion.java
+++ app/src/main/java/com/example/david/demo/Publicacion.java
@@ -4,11 +4,17 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -18,12 +24,11 @@ public class Publicacion extends AppCompatActivity {
 
     private String nombre;
     private String materia;
-    private String comentario;
     private String Calificacion;
-    private String color;
     private String mostrar;
     private LinearLayout fondo;
-    private TextView Pub;
+    private EditText Pub;
+    private ToggleButton color;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -31,44 +36,42 @@ public class Publicacion extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_publicacion);
         fondo = (LinearLayout) findViewById(R.id.fondo);
-        Pub = (TextView) findViewById(R.id.txtPublica);
+        Pub = (EditText) findViewById(R.id.txtPublica);
         String nomb = getIntent().getStringExtra("nombre");
-        String mat = getIntent().getStringExtra("Materia");
-        String coment = getIntent().getStringExtra("Comentario");
         String Cal = getIntent().getStringExtra("Calificacion");
-        String col = getIntent().getStringExtra("Color");
-        String most = getIntent().getStringExtra("mostrar");
+        color = (ToggleButton) findViewById(R.id.tgbColor);
+        String most = getIntent().getStringExtra("Mostrar");
         nombre = nomb;
-        materia = mat;
-        comentario = coment;
+
+
         Calificacion = Cal;
-        color = col;
+
         mostrar = most;
-        setcolor(color);
-        publicacion(nombre, materia, Calificacion, color, mostrar, Pub);
+        System.out.println(mostrar);
+        // setcolor(color);
+        // publicacion(nombre, materia, Calificacion, color, mostrar, Pub);
 
+                Pub.setText("Alumno: " + nombre + " Calificacion: " +Calificacion);
+
+        this.color.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+
+                    fondo.setBackgroundColor(Color.BLUE);
+                } else {
+                    // The toggle is disabled
+                    fondo.setBackgroundColor(Color.WHITE);
+                }
+            }
+        });
 
     }
 
-    public void publicacion(String nombre, String materia, String calificacion, String color, String mostrar, TextView pub) {
-        System.out.print(color);
-        Pub.setText("Alumno: " + nombre + " de la materia" +   " obtuvo " + calificacion+" de calificacion");
 
 
-    }
-
-    public void setcolor(String color) {
-        if (color == "A") {
-            fondo.setBackgroundColor(Color.BLUE);
-
-        } else {
-            fondo.setBackgroundColor(Color.WHITE);
-
-        }
-    }
 
 
 }
