b81a3541693bb89e0606e9273e691b17b791c4e4
domnoxx <d-avis89@hotmail.es>
2016-06-20 23:29:34 -0500

    Update

--- app/src/main/java/com/example/david/demo/MainActivity.java
+++ app/src/main/java/com/example/david/demo/MainActivity.java
@@ -6,15 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RatingBar;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText txtNombre;
     private EditText txtMateria;
     private AutoCompleteTextView atxtComentario;
-    private RatingBar ratingBar;
+    private RatingBar calificacion;
+    private ToggleButton color;
+    private CheckBox ckbMostrar;
     private Button btnEnviar;
 
     @Override
@@ -24,8 +28,9 @@ public class MainActivity extends AppCompatActivity {
         txtNombre = (EditText) findViewById(R.id.txtNombre);
         txtMateria= (EditText)findViewById(R.id.txtMateria);
         atxtComentario= (AutoCompleteTextView) findViewById(R.id.aTxtComentario);
-        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
-
+        calificacion = (RatingBar) findViewById(R.id.ratingBar);
+        ckbMostrar=(CheckBox) findViewById(R.id.ckbRating);
+        color=(ToggleButton)findViewById(R.id.tgbColor);
     }
 
 
@@ -34,7 +39,11 @@ public class MainActivity extends AppCompatActivity {
         demo.putExtra("nombre", txtNombre.getText().toString());
         demo.putExtra("Materia", txtMateria.getText().toString());
         demo.putExtra("Comentario", atxtComentario.getText().toString());
+        demo.putExtra("Calificacion",String.valueOf(calificacion.getRating()));
+        demo.putExtra("Color",color.getText());
+        demo.putExtra("mostrar", ckbMostrar.getText().toString());
         startActivity(demo);
+
     }
 
 
--- app/src/main/java/com/example/david/demo/Publicacion.java
+++ app/src/main/java/com/example/david/demo/Publicacion.java
@@ -2,12 +2,36 @@ package com.example.david.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Publicacion extends AppCompatActivity {
+
+    private String nombre;
+    private String materia;
+    private String comentario;
+    private String Calificacion;
+    private String color;
+    private String mostrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publicacion);
+
+
+        String nomb = getIntent().getStringExtra("nombre");
+        String mat = getIntent().getStringExtra("Materia");
+        String coment = getIntent().getStringExtra("Comentario");
+        String Cal = getIntent().getStringExtra("Calificacion");
+        String col = getIntent().getStringExtra("Color");
+        String most = getIntent().getStringExtra("mostrar");
+        nombre=nomb;
+        materia=mat;
+        comentario=coment;
+        Calificacion=Cal;
+        color=col;
+        mostrar=most;
+
+
     }
 }
